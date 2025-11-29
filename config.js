d34a2f0bb3fa162f2da92d376e5f1a64fe795b8d
Kavishka <kavishkacitrus@gmail.com>
2025-11-29 23:05:52 +0530

    add help dashboard

---
+++ config.js
@@ -0,0 +1,10 @@
+// This file should be added to .gitignore
+const firebaseConfig = {
+  apiKey: "AIzaSyDxsBQKLfoG-LhX2FlUsKmXCSzqgGb2qtw",
+  authDomain: "sahaya-abbe1.firebaseapp.com",
+  projectId: "sahaya-abbe1",
+  storageBucket: "sahaya-abbe1.firebasestorage.app",
+  messagingSenderId: "488435769082",
+  appId: "1:488435769082:web:ebcc1e66d87a4002887343",
+  measurementId: "G-KGJEVFHX2Y"
+};
